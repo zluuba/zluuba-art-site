20843aae8321d02e8e6beff2840441b7cd7f4f5c
zluuba <zluyba.nikitina@gmail.com>
2024-05-01 13:49:50 +0300

    fix(js): add different probabilities to get different headers

--- zluuba_art/static/js/greeting.js
+++ zluuba_art/static/js/greeting.js
@@ -1,18 +1,32 @@
 var greetings = [
-    "こんにちは",        // Japanese
-    "안녕하세요",        // Korean
-    "Bonjour",        // French
-    "Привіт",         // Ukrainian
-    "Hello",          // English
-    "Hallo",          // German
-    "Salut",          // Romanian
-    "Olá",            // Portuguese
+    { greeting: "こんにちは", weight: 7 },     // Japanese
+    { greeting: "안녕하세요", weight: 2 },     // Korean
+    { greeting: "Bonjour", weight: 1 },     // French
+    { greeting: "Привіт", weight: 1 },      // Ukrainian
+    { greeting: "Hello", weight: 1 },       // English
+    { greeting: "Hallo", weight: 1 },       // German
+    { greeting: "Salut", weight: 1 },       // Romanian
+    { greeting: "Olá", weight: 1 },         // Portuguese
 ];
 
 function changeGreeting() {
-    var randomIndex = Math.floor(Math.random() * greetings.length);
-    var randomGreeting = greetings[randomIndex];
-    document.getElementById("greeting").textContent = randomGreeting;
+    var totalWeight = greetings.reduce((acc, curr) => acc + curr.weight, 0);
+    var randomWeight = Math.random() * totalWeight;
+
+    var chosenGreeting;
+    var accumulatedWeight = 0;
+
+    greetings.some(function(greetingObj) {
+        accumulatedWeight += greetingObj.weight;
+        if (randomWeight < accumulatedWeight) {
+            chosenGreeting = greetingObj.greeting;
+            return true;
+        }
+    });
+
+    if (chosenGreeting.length) {
+        document.getElementById("greeting").textContent = chosenGreeting;
+    }
 }
 
 changeGreeting();

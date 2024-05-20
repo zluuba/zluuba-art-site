439dd7a9d02a49a2cbd063694a7a7fbfe110c6e1
zluuba <zluyba.nikitina@gmail.com>
2024-05-20 13:19:31 +0300

    feat: add file for constants

---
+++ zluuba_art/constants.py
@@ -0,0 +1,7 @@
+PET_PROJECTS = {
+    # webpage path : name of html file
+    'zluuba-art-website': 'zluuba_art_website.html',
+    'games-of-terminal': 'games_of_terminal.html',
+    'task-manager': 'task_manager.html',
+    'page-analyzer': 'page_analyzer.html',
+}

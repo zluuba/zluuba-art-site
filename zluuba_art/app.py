f101db67c7a1a64620ddc057c8844cd84993ad8a
zluuba <zluyba.nikitina@gmail.com>
2024-05-21 10:53:52 +0300

    feat(app): rebuild app using class-based views

--- zluuba_art/app.py
+++ zluuba_art/app.py
@@ -1,120 +1,143 @@
 from flask import (
-    abort, Flask, request, Response,
+    abort, Flask, typing as ft,
     render_template, send_from_directory,
+    # request,
 )
-from werkzeug.exceptions import HTTPException
+from flask.views import View
+from typing import Any
 
-from .constants import PET_PROJECTS
-from .logger import log
+from zluuba_art.constants import PET_PROJECTS
+from zluuba_art.error_handlers import handle_404, handle_generic_exception
+# from zluuba_art.logger.logger import log
+
+
+class HomeView(View):
+    methods = ['GET']
+    init_every_request = False
+
+    def dispatch_request(self) -> ft.ResponseReturnValue:
+        """
+        Handles the main page route, logs user statistics,
+        and renders the main page.
+
+        :return: The rendered 'index.html' template.
+        """
+
+        # log('users_statistic', request)
+        return render_template('index.html')
+
+
+class CVPageView(View):
+    methods = ['GET']
+    init_every_request = False
+
+    def dispatch_request(self) -> ft.ResponseReturnValue:
+        """
+        Renders the cv page.
+
+        :return: The rendered 'cv.html' template.
+        """
+
+        return render_template('cv.html')
+
+
+class ProjectsPageView(View):
+    methods = ['GET']
+    init_every_request = False
+
+    def dispatch_request(self) -> ft.ResponseReturnValue:
+        """
+        Renders the pet projects page.
+
+        :return: The rendered 'pet_projects.html' template.
+        """
+
+        return render_template('pet_projects.html')
+
+
+class ProjectPageView(View):
+    methods = ['GET']
+    init_every_request = False
+
+    def dispatch_request(self, *args: Any, **kwargs: Any
+                         ) -> ft.ResponseReturnValue:
+        """
+        Renders the specified pet project page.
+
+        :param project: Name of the pet project.
+        :return: The rendered project template.
+        """
+        project = kwargs['project']
+        project_html_page = PET_PROJECTS.get(project)
+
+        if not project_html_page:
+            abort(404)
+
+        return render_template(f'projects/{project_html_page}')
+
+
+class LinksPageView(View):
+    methods = ['GET']
+    init_every_request = False
+
+    def dispatch_request(self) -> ft.ResponseReturnValue:
+        """
+        Renders the links page.
+
+        :return: The rendered 'links.html' template.
+        """
+
+        return render_template('links.html')
+
+
+class RobotsPageView(View):
+    methods = ['GET']
+    init_every_request = False
+
+    def dispatch_request(self) -> ft.ResponseReturnValue:
+        """
+        Serves the robots.txt file.
+
+        :return: The robots.txt file.
+        """
+
+        static_folder = app.static_folder
+
+        if static_folder is None:
+            abort(404)
+
+        return send_from_directory(static_folder, 'robots.txt')
+
+
+def register_api(app):
+    app.add_url_rule(
+        '/',
+        view_func=HomeView.as_view('main_page'),
+    )
+    app.add_url_rule(
+        '/cv',
+        view_func=CVPageView.as_view('cv_page'),
+    )
+    app.add_url_rule(
+        '/projects',
+        view_func=ProjectsPageView.as_view('projects_page')
+    )
+    app.add_url_rule(
+        '/projects/<project>',
+        view_func=ProjectPageView.as_view('project_page')
+    )
+    app.add_url_rule(
+        '/links',
+        view_func=LinksPageView.as_view('links_page')
+    )
+    app.add_url_rule(
+        '/robots.txt',
+        view_func=RobotsPageView.as_view('robots_page')
+    )
+
+    app.register_error_handler(404, handle_404)
+    app.register_error_handler(Exception, handle_generic_exception)
 
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def main_page() -> Response:
-    """
-    Handles the main page route, logs user statistics,
-    and renders the main page.
-
-    :return: The rendered 'index.html' template.
-    """
-
-    log('users_statistic', request)
-    return render_template('index.html')
-
-
-@app.route('/cv')
-def cv_page() -> Response:
-    """
-    Renders the cv page.
-
-    :return: The rendered 'cv.html' template.
-    """
-
-    return render_template('cv.html')
-
-
-@app.route('/projects')
-def projects_page() -> Response:
-    """
-    Renders the pet projects page.
-
-    :return: The rendered 'pet_projects.html' template.
-    """
-
-    return render_template('pet_projects.html')
-
-
-@app.route('/projects/<project>')
-def project_page(project: str) -> Response:
-    """
-    Renders the specified pet project page.
-
-    :param project: Name of the pet project.
-    :return: The rendered project template.
-    """
-
-    project_html_page = PET_PROJECTS.get(project)
-
-    if not project_html_page:
-        abort(404)
-
-    return render_template(f'projects/{project_html_page}')
-
-
-@app.route('/links')
-def links_page() -> Response:
-    """
-    Renders the links page.
-
-    :return: The rendered 'links.html' template.
-    """
-
-    return render_template('links.html')
-
-
-@app.route('/robots.txt')
-def robots_page() -> Response:
-    """
-    Renders the robots.txt page.
-
-    :return: The robots.txt file.
-    """
-
-    return send_from_directory(app.static_folder, 'robots.txt')
-
-
-@app.errorhandler(404)
-def page_not_found(error: HTTPException) -> Response:
-    """
-    Handles Page Not Found error (404).
-
-    :return: The rendered 'page_not_found.html' template.
-    """
-
-    return render_template('errors/page_not_found.html'), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(error: HTTPException) -> Response:
-    """
-    Handles Internal Server Error (500), logs this error.
-
-    :return: The rendered 'internal_server_error.html' template.
-    """
-
-    log('server_error', error)
-    return render_template('errors/internal_server_error.html'), 500
-
-
-@app.errorhandler(Exception)
-def internal_server_error(error: Exception) -> Response:
-    """
-    Handles server error (any 500's error), logs this error.
-
-    :return: The rendered 'internal_server_error.html' template.
-    """
-
-    log('server_error', error)
-    return render_template('errors/error.html'), 500
+register_api(app)
